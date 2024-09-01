29117ce7e212ecdd543574db94e8b684d03402b6
reaovyd <zackbellzokobell@gmail.com>
2024-09-01 16:47:36 -0700

    feat(request): add related guilds payload

---
+++ src/payload/request/guild.rs
@@ -0,0 +1,88 @@
+use crate::payload::request::macros::make_request_payload;
+use derive_builder::Builder;
+use paste::paste;
+use serde::Serialize;
+use serde_with::skip_serializing_none;
+use uuid::Uuid;
+
+make_request_payload!(
+    GetGuild,
+    (guild_id, String, "Id of the guild to get"),
+    (timeout, Option<u32>, "asynchronously get guild with time to wait before timing out")
+);
+make_request_payload!(GetGuilds);
+
+#[cfg(test)]
+mod get_guild_tests {
+    use pretty_assertions::assert_eq;
+
+    use super::{
+        GetGuild,
+        GetGuildArgsBuilder,
+    };
+
+    #[test]
+    fn test_construction_basic() {
+        let guild = GetGuild::new(
+            GetGuildArgsBuilder::create_empty()
+                .guild_id("guild_id".to_string())
+                .timeout(Some(32))
+                .build()
+                .unwrap(),
+        );
+        assert_eq!(guild.args.guild_id, "guild_id".to_string());
+        assert_eq!(guild.args.timeout, Some(32));
+    }
+
+    #[test]
+    fn test_serialization_timeout_empty() {
+        let guild = GetGuild::new(
+            GetGuildArgsBuilder::create_empty()
+                .guild_id("guild_id".to_string())
+                .timeout(None)
+                .build()
+                .unwrap(),
+        );
+
+        assert_eq!(guild.args.timeout, None);
+
+        let serialized = serde_json::to_string(&guild).unwrap();
+        assert!(!serialized.contains("\"timeout\":"))
+    }
+
+    #[test]
+    fn test_serialization_timeout_filled() {
+        let guild = GetGuild::new(
+            GetGuildArgsBuilder::create_empty()
+                .guild_id("guild_id".to_string())
+                .timeout(Some(32))
+                .build()
+                .unwrap(),
+        );
+
+        assert_eq!(guild.args.timeout, Some(32));
+
+        let serialized = serde_json::to_string(&guild).unwrap();
+        assert!(serialized.contains("\"timeout\":32"))
+    }
+}
+
+#[cfg(test)]
+mod get_guilds_tests {
+    use pretty_assertions::assert_eq;
+
+    use super::GetGuilds;
+
+    #[test]
+    fn test_construction_basic() {
+        let guilds = GetGuilds::new();
+        assert_eq!(guilds.args.inner, None);
+    }
+
+    #[test]
+    fn test_serialization_empty_args() {
+        let guilds = GetGuilds::new();
+        let serialized = serde_json::to_string(&guilds).unwrap();
+        assert!(serialized.contains("\"args\":{}"))
+    }
+}
--- src/payload/request/mod.rs
+++ src/payload/request/mod.rs
@@ -1,11 +1,35 @@
 mod macros {
     macro_rules! make_request_payload {
-        ($request_name: ident, $(($field_name: ident, $field_type: ty, $field_doc: expr)),+) => {
-            use derive_builder::Builder;
-            use paste::paste;
-            use serde::Serialize;
-            use uuid::Uuid;
+        ($request_name: ident) => {
+            #[derive(Debug, Clone, Serialize)]
+            pub struct $request_name {
+                nonce: Uuid,
+                args: EmptyArgs 
+            }
 
+            impl $request_name {
+                pub fn new() -> Self {
+                    Self {
+                        nonce: Uuid::new_v4(),
+                        args: EmptyArgs::default() 
+                    }
+                }
+            }
+
+            impl Default for $request_name {
+                fn default() -> Self {
+                    Self::new()
+                }
+            }
+
+            #[derive(Debug, Clone, Serialize, PartialEq, Eq, Hash, Default)]
+            pub struct EmptyArgs {
+                #[serde(flatten)] 
+                #[serde(skip_serializing_if = "Option::is_none")]
+                inner: Option<()>, 
+            }
+        };
+        ($request_name: ident, $(($field_name: ident, $field_type: ty, $field_doc: expr)),*) => {
             #[derive(Debug, Clone, Serialize)]
             pub struct $request_name {
                 nonce: Uuid,
@@ -13,6 +37,7 @@ mod macros {
             }
 
             paste! {
+                #[skip_serializing_none]
                 #[derive(Builder, Debug, Clone, Serialize, PartialEq, Eq, Hash)]
                 #[builder(derive(Debug))]
                 // lint isn't catching a /**/ comment where the Errors section is according to
@@ -22,7 +47,7 @@ mod macros {
                     $(
                         #[doc = $field_doc]
                         $field_name: $field_type
-                    ),+
+                    ),*
                 }
             }
 
@@ -39,5 +64,6 @@ mod macros {
     }
     pub(crate) use make_request_payload;
 }
+pub use guild::*;
 
-use macros::make_request_payload;
+mod guild;
