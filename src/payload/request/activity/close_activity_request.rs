632a1c976ab584f1146072c2bcc29c5b0a95a890
reaovyd <zackbellzokobell@gmail.com>
2024-09-07 16:10:11 -0700

    test(request-activity): add two basic build tests for close_act and send_act

--- src/payload/request/activity/close_activity_request.rs
+++ src/payload/request/activity/close_activity_request.rs
@@ -8,3 +8,20 @@ make_request_payload!(CloseActivityRequest,
     #[doc = "Used to reject an Ask to Join request."],
     (user_id, String, "The id of the requesting user")
 );
+
+#[cfg(test)]
+mod tests {
+    use super::{
+        CloseActivityRequest,
+        CloseActivityRequestArgsBuilder,
+    };
+
+    #[test]
+    fn test_build_request() {
+        let request = CloseActivityRequest::new(
+            CloseActivityRequestArgsBuilder::default().user_id("asd".to_owned()).build().unwrap(),
+        );
+        let request = serde_json::to_string(&request).unwrap();
+        assert!(request.contains(r#""user_id":"asd""#))
+    }
+}
--- src/payload/request/activity/send_activity_join_invite.rs
+++ src/payload/request/activity/send_activity_join_invite.rs
@@ -8,3 +8,17 @@ make_request_payload!(SendActivityJoinInvite,
     #[doc = "Used to accept an Ask to Join request."],
     (user_id, String, "The id of the requesting user")
 );
+
+#[cfg(test)]
+mod tests {
+    use crate::payload::request::{SendActivityJoinInvite, SendActivityJoinInviteArgsBuilder};
+
+    #[test]
+    fn test_build_request() {
+        let request = SendActivityJoinInvite::new(
+            SendActivityJoinInviteArgsBuilder::default().user_id("abcjoe".to_owned()).build().unwrap(),
+        );
+        let request = serde_json::to_string(&request).unwrap();
+        assert!(request.contains(r#""user_id":"abcjoe""#))
+    }
+}
