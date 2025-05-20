81fa0966c17d7068f8a40be2adb7f731d5e758fb
reaovyd <zackbellzokobell@gmail.com>
2025-05-20 01:05:03 -0700

    feat(examples): add basic custom activity example

---
+++ examples/custom_activity.rs
@@ -0,0 +1,68 @@
+use std::time::Duration;
+
+use anyhow::Result;
+use sdkcord::{
+    client::SdkClient,
+    config::{Config, OAuth2Config},
+    payload::{
+        SetActivityArgs,
+        common::{
+            activity::{Activity, ActivityType, Assets, Party},
+            oauth2::OAuth2Scope,
+        },
+    },
+};
+use tokio::time::sleep;
+use tracing::info;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let subscriber = tracing_subscriber::FmtSubscriber::new();
+    tracing::subscriber::set_global_default(subscriber)?;
+    let scopes = [
+        OAuth2Scope::Rpc,
+        OAuth2Scope::Identify,
+        OAuth2Scope::Guilds,
+        OAuth2Scope::MessagesRead,
+        OAuth2Scope::RpcNotificationsRead,
+    ];
+    let client = SdkClient::new(
+        Config::default(),
+        "<YOUR_CLIENT_ID_HERE>",
+        Some(
+            OAuth2Config::builder()
+                .client_secret("<YOUR_CLIENT_SECRET_HERE>")
+                .scopes(scopes)
+                .build(),
+        ),
+    )
+    .await?;
+    let activity = client
+        .set_activity(
+            SetActivityArgs::builder()
+                .pid(std::process::id())
+                .activity(
+                    Activity::request_builder()
+                        .activity_type(ActivityType::Playing)
+                        .details("osaka")
+                        .assets(
+                            Assets::builder()
+                                .large_text("osaka")
+                                .small_text("osaka")
+                                .large_image("yoru")
+                                .small_image("yoru")
+                                .build(),
+                        )
+                        .state("hiii")
+                        .party(Party::builder().size([12, 24]).build())
+                        .call(),
+                )
+                .build(),
+        )
+        .await;
+    info!("{:?}", activity);
+    // infinite loop so activity can remain showing on discord
+    loop {
+        sleep(Duration::from_secs(60 * 60)).await
+    }
+}
